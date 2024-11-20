65e201d4b4943ea43f2eed2c856a42500963a5dc
Varep <80421433+Varep@users.noreply.github.com>
2024-11-20 21:43:11 +0300

    Add files via upload

---
+++ src/client/java/ru/varep/localtrader/client/LocaltraderClient.java
@@ -0,0 +1,10 @@
+package ru.varep.localtrader.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LocaltraderClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/ru/varep/localtrader/Localtrader.java
@@ -0,0 +1,10 @@
+package ru.varep.localtrader;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Localtrader implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
